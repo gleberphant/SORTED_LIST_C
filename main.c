e65fcdca3bc7d8efbdba1a8ab474408bafa124d3
Handerson Gleber <handerson.gleber@gmail.com>
2024-11-15 14:35:34 -0300

    modularizacao

--- main.c
+++ main.c
@@ -1,70 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef struct NODE NODE;
-
-struct NODE
-{
-    int number;
-    NODE *next;
-};
-
-
-/* Função que libera a memoria alocada dinamicamente
-  * 
- */
-void freeNode(NODE* node){
-    
-    // quando for o ultimo no retorna sem fazer nada   
-    if(node == NULL) return;
-
-    // chama o proximo n
-    freeNode(node->next);
-    
-    // quando retornar da função que liberou o proximo libera o nó
-    printf("*%d ", node->number);
-    free(node);
-}
-
-/* Função que imprime uma lista
- * 
- */
-void printList(NODE* listHead){
-    
-    printf("\n Lista Organizada: "  );
-
-    for(NODE* current = listHead; current != NULL; current = current->next){
-        printf(" < %d", current->number);
-    }
-    
-}
-
-/* Função que percorre a lista e insere o novo nó na posição correta
- * recebe um ponteiro para o listHead e o novo Nó
- * 
- */
-void insertNode(NODE** listHead, NODE* newNode ){
-   
-    NODE **current;   
-
-    // PERCORRE A LISTA PARA ENCONTRAR A POSIÇÃO CORRETA
-    for(current = listHead ; *current != NULL ; current = &(*current)->next ){
-        // quando o novo nó for menor que o nó  atual,  quebra o laço
-        if( newNode->number < (*current)->number ){
-            break;
-        }       
-        // se o novo nó for maior que o nó atual então avança a lista
-    }
-    // define que nó seguinte do novo nó é o da posição atual
-    newNode->next = *current;
-
-    // define que a posição atual agora passa a ser o novo nó
-    *current = newNode;
-
-    // finaliza a função
-    return;
-}
+#include "nodes.c"
 
 
 int main(int argc, char *argv[])
@@ -101,7 +35,7 @@ int main(int argc, char *argv[])
     printf("\n ---------------------------------");
     printf("\n Liberando memoria:  ");
 
-    freeNode(listHead);
+    freeNodeRecursive(listHead);
 
     return 0;
 }
---
+++ nodes.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct NODE NODE;
+
+struct NODE
+{
+    int number;
+    NODE *next;
+};
+
+
+/* Função que libera a memoria alocada dinamicamente
+  * 
+ */
+void freeNodeRecursive(NODE* node){
+    
+    // quando for o ultimo no retorna sem fazer nada   
+    if(node == NULL) return;
+
+    // chama o proximo n
+    freeNodeRecursive(node->next);
+    
+    // quando retornar da função que liberou o proximo libera o nó
+    printf("*%d ", node->number);
+    free(node);
+}
+
+/* Função que imprime uma lista
+ * 
+ */
+void printList(NODE* listHead){
+    
+    printf("\n Lista Organizada: "  );
+
+    for(NODE* current = listHead; current != NULL; current = current->next){
+        printf(" < %d", current->number);
+    }
+    
+}
+
+/* Função que percorre a lista e insere o novo nó na posição correta
+ * recebe um ponteiro para o listHead e o novo Nó
+ * 
+ */
+void insertNode(NODE** listHead, NODE* newNode ){
+   
+    NODE **current;   
+
+    // PERCORRE A LISTA PARA ENCONTRAR A POSIÇÃO CORRETA
+    for(current = listHead ; *current != NULL ; current = &(*current)->next ){
+        // quando o novo nó for menor que o nó  atual,  quebra o laço
+        if( newNode->number < (*current)->number ){
+            break;
+        }       
+        // se o novo nó for maior que o nó atual então avança a lista
+    }
+    // define que nó seguinte do novo nó é o da posição atual
+    newNode->next = *current;
+
+    // define que a posição atual agora passa a ser o novo nó
+    *current = newNode;
+
+    // finaliza a função
+    return;
+}
